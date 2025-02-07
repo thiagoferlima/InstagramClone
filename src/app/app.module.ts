c19f0caf7ccc1310016a213ae5c2364b5636d52e
thiago-fernandes <thicodes.com>
2025-02-07 19:41:21 -0300

    Ajustando o componente Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
 import { BannerComponent } from './acesso/banner/banner.component';
+import { LoginComponent } from './acesso/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AcessoComponent,
     BannerComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule
