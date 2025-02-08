069ed1dca9e923b2d8f80e5eb625d2f1aad72077
thiago-fernandes <thicodes.com>
2025-02-08 16:36:54 -0300

    Estado especial void e estilos na transição de estados - Login

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -17,12 +17,26 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         animate('500ms 0s ease-in-out') 
       ])
       
+    ]),
+    trigger('animacao-painel',[
+      state('criado', style({
+        opacity: 1
+
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(50px, 0)' }),
+        animate('500ms 0s ease-in-out') 
+
+      ])
+
     ])
   ]
 })
 export class AcessoComponent implements OnInit {
 
   public estadoBanner: string= 'criado'
+
+  public estadoPainel:string = 'criado'
 
   constructor() { }
 
