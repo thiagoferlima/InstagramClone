a92958864549557758c7897a156da297ff4314ce
thiago-fernandes <thicodes.com>
2025-02-08 09:40:13 -0300

    Animations, triggers, states e styles

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity:1
+      }))
+
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+  public estado: string = 'visivel'
 
   constructor() { }
 
