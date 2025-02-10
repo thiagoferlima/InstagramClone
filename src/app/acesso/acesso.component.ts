ef1a5b00a80f597116191532a1ef03bc432c50a4
thiago-fernandes <thicodes.com>
2025-02-10 11:27:28 -0300

    (Login e Cadastro) com keyframes

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 
 @Component({
@@ -25,7 +25,23 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       })),
       transition('void => criado', [
         style({ opacity: 0, transform: 'translate(50px, 0)' }),
-        animate('500ms 0s ease-in-out') 
+
+
+        animate('1.5s 0s ease-in-out', keyframes([
+          style({ offset: 0.15, opacity: 1, transform: 'translateX(0)'}),
+          style({ offset: 0.86, opacity: 1, transform: 'translateX(0)'}),
+
+          style({ offset: 0.88, opacity: 1, transform: 'translateY(-10px)'}),
+          style({ offset: 0.90, opacity: 1, transform: 'translateX(10px)'}),
+          style({ offset: 0.92, opacity: 1, transform: 'translateX(-10px)'}),
+          style({ offset: 0.94, opacity: 1, transform: 'translateX(10px)'}),
+          style({ offset: 0.96, opacity: 1, transform: 'translateX(-10px)'}),
+          style({ offset: 0.98, opacity: 1, transform: 'translateX(10px)'}),
+
+
+          style({ offset: 1, opacity: 1, transform: 'translateX(0)'})
+
+        ])) 
 
       ])
 
