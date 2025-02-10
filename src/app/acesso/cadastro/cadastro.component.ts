301b7b293fa9438434d0636d6ad180bc8f72bb45
thiago-fernandes <thicodes.com>
2025-02-10 10:25:01 -0300

    Output (EventEmitter)  (CadastroComponent)

--- src/app/acesso/cadastro/cadastro.component.ts
+++ src/app/acesso/cadastro/cadastro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
+  @Output() public exibirPainel: EventEmitter<string> = new EventEmitter<string>() 
 
   constructor() { }
 
   ngOnInit() {
+  }
+  public exibirPainelLogin(): void{
+    this.exibirPainel.emit('login')
+
   }
 
 }
